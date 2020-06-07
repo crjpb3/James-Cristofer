9624c50053105f48262ab5e402eebd8c7a782327
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2020-06-07 12:47:50 -0500

    Mapping Example second update

--- Mapping Example/Mapping Example/Main.cpp
+++ Mapping Example/Mapping Example/Main.cpp
@@ -9,8 +9,18 @@ using namespace std;
 
 int main()
 {
+	map<string, string> example_map;
+	map<string, string>::iterator itr;
+	pair<string, string> map_pair;
+	map_pair.first = "Test";
+	map_pair.second = "Test again";
 
+	example_map.insert(map_pair);
 
+	for (itr = example_map.begin(); itr != example_map.end(); itr++)
+	{
+		cout << itr->first << ": " << itr->second << endl;
+	}
 
 	
 	return 0;
