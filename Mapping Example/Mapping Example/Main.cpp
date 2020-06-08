d75c4faea6c1aa44d3214b2d44bf9d55a0625b1b
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2020-06-08 10:10:31 -0500

    Mapping Example update 2

--- Mapping Example/Mapping Example/Main.cpp
+++ Mapping Example/Mapping Example/Main.cpp
@@ -9,17 +9,28 @@ using namespace std;
 
 int main()
 {
-	map<string, string> example_map;
-	map<string, string>::iterator itr;
-	pair<string, string> map_pair;
-	map_pair.first = "Test";
-	map_pair.second = "Test again";
+	map<string, double> transaction_map;
+	pair<string, double> pair1;
+	pair1.first = "Transaction Test";
+	pair1.second = 10.50;
+	transaction_map.insert(pair1);
+	map<string, map<string, double>> base_map;
+	map<string, map<string, double>>::iterator itr;
+	map<string, double>::iterator itr2;
+	pair<string, map<string, double>> pair2;
+	pair2.first = "Base Test";
+	pair2.second = transaction_map;
 
-	example_map.insert(map_pair);
+	base_map.insert(pair2);
 
-	for (itr = example_map.begin(); itr != example_map.end(); itr++)
+
+	for (itr = base_map.begin(); itr != base_map.end(); itr++)
 	{
-		cout << itr->first << ": " << itr->second << endl;
+		cout << itr->first << ": ";
+		for (itr2 = transaction_map.begin(); itr2 != transaction_map.end(); itr2++)
+		{
+			cout << itr2->first << ": " << itr2->second << endl;
+		}
 	}
 
 	
