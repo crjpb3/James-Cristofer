401bec0392f5346643cc98cc00fec2632e4c78cf
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2020-06-10 17:44:56 -0500

    Final update for Mapping Example

--- Mapping Example/Mapping Example/Main.cpp
+++ Mapping Example/Mapping Example/Main.cpp
@@ -1,28 +1,25 @@
 //Cristofer James
 //June 3, 2020
 #include <iostream>
-#include <string>
-#include <vector>
 #include <iterator>
 #include <map>
 using namespace std;
 
 int main()
-{
+{//Mainly experimenting with using a map inside a map
+	map<string, map<string, double>> base_map;
+	map<string, map<string, double>>::iterator itr;
 	map<string, double> transaction_map;
+	map<string, double>::iterator itr2;
 	pair<string, double> pair1;
+	pair<string, map<string, double>> pair2;
+
 	pair1.first = "Transaction Test";
 	pair1.second = 10.50;
 	transaction_map.insert(pair1);
-	map<string, map<string, double>> base_map;
-	map<string, map<string, double>>::iterator itr;
-	map<string, double>::iterator itr2;
-	pair<string, map<string, double>> pair2;
 	pair2.first = "Base Test";
 	pair2.second = transaction_map;
-
 	base_map.insert(pair2);
-
 
 	for (itr = base_map.begin(); itr != base_map.end(); itr++)
 	{
