2d2d5e41f509e6dc158f22fc6faf818629058bc6
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2020-06-12 09:49:46 -0500

    Structured version 'File Reading and Writing'

---
+++ File Reading and Writing/File Reading and Writing/File_Handling.h
@@ -0,0 +1,16 @@
+//Cristofer James
+//June 12, 2020
+#pragma once
+#ifndef FILE_HANDLING_H
+#define FILE_HANDLING_H
+
+class File_Handling
+{
+public:
+
+
+private:
+
+
+};
+#endif
---
+++ File Reading and Writing/File Reading and Writing/File_Handling_IMP.cpp
@@ -0,0 +1,5 @@
+//Cristofer James
+//June 12, 2020
+#include <iostream>
+#include <fstream>
+using namespace std;
