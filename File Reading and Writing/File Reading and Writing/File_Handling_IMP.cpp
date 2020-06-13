06e923370cbd47c8c4fc5183df9b3c5b3a0dfda8
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2020-06-13 10:22:45 -0500

    6/13/20 Update

--- File Reading and Writing/File Reading and Writing/File_Handling_IMP.cpp
+++
@@ -1,5 +0,0 @@
-//Cristofer James
-//June 12, 2020
-#include <iostream>
-#include <fstream>
-using namespace std;
--- File Reading and Writing/File Reading and Writing/Main.cpp
+++ File Reading and Writing/File Reading and Writing/Main.cpp
@@ -1,11 +1,42 @@
 //Cristofer James
 //June 10, 2020
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
+	ifstream fin;
+	ofstream fout;
+	string name;
+	int age_years;
+	char choice;
 
+	cout << "A - Display stored info" << endl;
+	cout << "B - Enter info to store" << endl;
+	cin >> choice;
+	if (toupper(choice) == 'A')
+	{
+		fin.open(filename);
+		fin >> name;
+		fin >> age_years;
+		fin.close(filename);
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age_years << endl;
+	}
+	else
+	{
+		fout.open(filename);
+		cout << "Enter name: ";
+		cin >> name;
+		cout << "Enter age: ";
+		cin >> age_years;
+		fout << name << endl;
+		fout << age_years << endl;
+		fout.close(filename);
+	}
+	system("cls");
+	cout << "Operations Complete" << endl;
 
 
 
