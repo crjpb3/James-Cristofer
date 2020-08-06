0750fcf43d723fc74f8fe613d997bd854f81bf8c
Cristofer James <crjpb3@mail.umkc.edu>
2020-08-06 15:20:46 -0500

    First Python Program; Basic Hello World

---
+++ Python Programs/Program 1/main.py
@@ -0,0 +1,24 @@
+#Cristofer James
+#August 6, 2020
+#First Python program
+
+print('Hello World')
+
+first = 'Hello'
+second = 'World'
+
+print(first, second)
+print(first, first)
+print(second, second)
+print(second, first)
+
+num_one = 2
+num_two = 2
+
+print('First number: ', num_one)
+print('Second number: ', num_two)
+print(num_one, ' + ', num_two, ' = ', num_one + num_two)
+print(num_one, ' - ', num_two, ' = ', num_one - num_two)
+print(num_one, ' * ', num_two, ' = ', num_one * num_two)
+print(num_one, ' / ', num_two, ' = ', num_one / num_two)
+print(num_one, ' ^ ', num_two, ' = ', num_one ** num_two)
